357d66ff5873f8ef0189c78620bcdeddd0cc18af
Dina <dina.rotenberg483@gmail.com>
2021-11-09 18:37:14 +0300

    test Cat

--- core/src/com/mygdx/game/Cat.java
+++ core/src/com/mygdx/game/Cat.java
@@ -12,26 +12,27 @@ public class Cat {
     float vy;
     float gravity;
 
+
     public Cat(){
         img = new Texture("cat.png");
         position = new Vector2(100,380);
         vy = 0;
         gravity = - 0.7f;
     }
-    public void render(SpriteBatch batch){
 
-        batch.draw(img, position.x,position.y);
+    public void render(SpriteBatch batch){
+        batch.draw(img, position.x, position.y);
     }
 
     public void update(){
 
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
             vy = 10;
         }
         vy += gravity;
         position.y += vy;
-
     }
+
     public void recreate(){
         position = new Vector2(100,380);
         vy = 0;
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -18,44 +18,62 @@ public class MyGdxGame extends ApplicationAdapter {
 	boolean gameOver;
 	Texture restartTexture;
 
+
+
 	@Override
-	public void create() {
+	public void create () {
 		batch = new SpriteBatch();
 		bg = new Background();
 		cat = new Cat();
 		obstacles = new Obstacles();
 		gameOver = false;
 		restartTexture = new Texture("gameover.png");
+
 	}
 
 	@Override
-	public void render() {
+	public void render () {
 		update();
-		Gdx.gl.glClearColor(1,1,1,1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		bg.render(batch);
-		cat.render(batch);
 		obstacles.render(batch);
-		if (!gameOver) {
+		if(!gameOver) {
 			cat.render(batch);
-		} else {
-			batch.draw(restartTexture, 200, 200);
+		}else{
+			batch.draw(restartTexture, 220, 280);
 		}
 		batch.end();
 	}
 
-	public void update() {
+	public void update(){
 		bg.update();
 		cat.update();
 		obstacles.update();
+		for (int i = 0; i < Obstacles.obs.length; i++) {
+			if(cat.position.x > Obstacles.obs[i].position.x && cat.position.x < Obstacles.obs[i].position.x+50){
+				if(!Obstacles.obs[i].emptySpace.contains(cat.position)){
+					gameOver = true;
+				}
+			}
+		}
+		if(cat.position.y <0 || cat.position.y > 600){
+			gameOver = true;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && gameOver){
+			recreate();
+		}
+
 
 	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 	}
-	public void recreate () {
+
+	public void recreate(){
 		cat.recreate();
 		obstacles.recreate();
 		gameOver = false;
