cafade416a85841224a163969a3453dc55c840d3
Dina <dina.rotenberg483@gmail.com>
2021-11-09 18:23:56 +0300

    test Obstacles

--- core/src/com/mygdx/game/Obstacles.java
+++ core/src/com/mygdx/game/Obstacles.java
@@ -9,24 +9,25 @@ import java.util.Random;
 
 public class Obstacles {
 
-    class WallPair {
+    class WallPair{
         Vector2 position;
         float speed;
         int offset;
         Rectangle emptySpace;
 
-        public WallPair(Vector2 pos) {
+
+        public WallPair(Vector2 pos){
             position = pos;
             speed = 2;
-            offset = new Random().nextInt(200);
+            offset = new Random().nextInt(250);
             emptySpace = new Rectangle(position.x, position.y - offset + 300, 50, betweenDistance);
         }
 
-        public void update() {
+        public void update(){
             position.x -= speed;
-            if (position.x < -50) {
+            if(position.x < - 50){
                 position.x = 800;
-                offset = new Random().nextInt(200);
+                offset = new Random().nextInt(250);
             }
             emptySpace.x = position.x;
         }
@@ -36,36 +37,35 @@ public class Obstacles {
     Texture txt;
     int betweenDistance;
 
-    public Obstacles() {
+    public Obstacles(){
         txt = new Texture("wall.png");
         obs = new WallPair[4];
         betweenDistance = 250;
         int startPosX = 400;
         for (int i = 0; i < obs.length; i++) {
-            obs[i] = new WallPair(new Vector2(startPosX, 0));
+            obs[i] = new WallPair(new Vector2(startPosX,0));
             startPosX += 220;
         }
+
     }
 
-    public void render(SpriteBatch batch) {
+    public void render(SpriteBatch batch){
         for (int i = 0; i < obs.length; i++) {
-            batch.draw(txt, obs[1].position.x, obs[i].position.y );
-            batch.draw(txt, obs[1].position.x, obs[i].position.y + betweenDistance + txt.getHeight() - obs [i].offset);
-
+            batch.draw(txt, obs[i].position.x, obs[i].position.y - obs[i].offset);
+            batch.draw(txt, obs[i].position.x, obs[i].position.y + betweenDistance + txt.getHeight() - obs[i].offset);
         }
     }
 
-    public void update() {
-        for (WallPair ob : obs) {
-            ob.update();
+    public void update(){
+        for (int i = 0; i < obs.length; i++) {
+            obs[i].update();
         }
-
     }
 
-    public void recreate() {
+    public void recreate(){
         int startPosX = 400;
         for (int i = 0; i < obs.length; i++) {
-            obs[i] = new WallPair(new Vector2(startPosX, 0));
+            obs[i] = new WallPair(new Vector2(startPosX,0));
             startPosX += 220;
         }
     }
